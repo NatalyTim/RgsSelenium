2d578faea20bb861c2bc403acba5155e5ea4bb5b
NatalyTim <nataly.sun89@mail.ru>
2019-03-17 09:34:04 +0300

    Second

--- src/ru/timofeeva/Main.java
+++ src/ru/timofeeva/Main.java
@@ -2,7 +2,9 @@ package ru.timofeeva;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -43,7 +45,7 @@ public class Main {
           //  WebElement element = driver.findElement(By.xpath("//div/span[@class='h1']"));
             compareText("//div/span[@class='h1']", expectString);
 
-            Thread.sleep(2000);
+           // Thread.sleep(2000);
 
             //14. Я согласен на обработку данных  - выбрать чекбокс
             System.out.println("5.5 Я согласен на обработку данных  - выбрать чекбокс");
@@ -105,34 +107,34 @@ public class Main {
             System.out.println("15. Нажать рассчитать");
             scrollToAndClickElement(By.xpath("//*[@data-test-name='NextButton'][contains(@data-bind,'Misc.NextButton')]"));
 
-//            16. Проверить значения:
-            Thread.sleep(2000);
+            //16. Проверить значения:
+            //Thread.sleep(2000);
             System.out.println("16.Проверить значения");
 
-//            //16.0 Условия страхования – Многократные поездки в течении года
+            //16.0 Условия страхования – Многократные поездки в течении года
             System.out.println("16.0 Условия страхования – Многократные поездки в течении года");
             String trips = "Многократные поездки в течение года";
             compareText("//span[contains(@class,'summary-value')][@data-bind='with: Trips']/span[@class='text-bold']", trips);
 
-//          16.1 Территория – Шенген
+          //16.1 Территория – Шенген
             System.out.println("16.1 Территория – Шенген");
             compareText("//span/span[contains(@data-bind,'foreach: countries')]/strong", "Шенген");
 
-//            16.2 Застрахованный
+           //16.2 Застрахованный
             System.out.println("16.2 Застрахованный");
             String fullName = "IVAN NIKOLAEVICH";
             compareText("//strong[contains(@data-bind,'LastName')]", fullName);
 
-//            16.3 Дата рождения
+           //16.3 Дата рождения
             System.out.println("16.3 Дата рождения");
             compareText("//strong[contains(@data-bind,' text: BirthDay.')]", "15.02.1966");
 
-//         16.4 Активный отдых - включен
+           //16.4 Активный отдых - включен
             System.out.println("16.4 Активный отдых - включен");
             compareText("//div[contains(@data-bind, 'SelectedProgram.Options')]" +
                     "/div[contains(@data-bind, 'Активный')]/div[@class='summary-row']/span[@class='summary-value']/span", "Включен");
 
-            Thread.sleep(10000);
+            //Thread.sleep(10000);
 
         } catch (Exception e) {
             System.out.println("The following exception was generated:");
@@ -145,10 +147,14 @@ public class Main {
     }
 
     public static void compareText(String xpath, String expect) throws Exception{
-        WebElement s1 = driver.findElement(By.xpath(xpath));
+       WebElement element = (new WebDriverWait(driver,10)
+               .until(ExpectedConditions.elementToBeClickable(By.xpath(xpath))));
+        //element = driver.findElement(By.xpath(xpath));
+
+        //WebElement s1 = driver.findElement(By.xpath(xpath));
         JavascriptExecutor jse = (JavascriptExecutor) driver;
-        jse.executeScript("arguments[0].scrollIntoView()", s1);
-        String s2 = s1.getText();
+        jse.executeScript("arguments[0].scrollIntoView()", element);
+        String s2 = element.getText();
         if (s2.contains(expect)) {
             System.out.println("Исходный текст есть: " + expect);
         } else {
